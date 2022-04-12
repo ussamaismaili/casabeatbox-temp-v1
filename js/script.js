73520060e8173492ca63014341dbb02f9edae395
Oussama Ismaili <ismailiossama@gmail.com>
2022-04-12 18:13:01 +0200

    initialcommit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let dropList = document.querySelector('.drop-icon');
+let links = document.querySelector('#nav');
+dropList.onclick=function() {
+    links.style.display ="block";
+    console.log("okay");
+};
