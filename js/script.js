f92757580e76da5d6527ae2bddfe67644fee2787
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-05 02:09:05 +0100

    hamburger icon fixed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,50 @@
 let dropList = document.querySelector('.drop-icon');
 let links = document.querySelector('#nav');
+
+document.querySelector('body').addEventListener('click' , function () {
+    var e= e || window.event;
+            e= e.target || e.srcElement;
+            
+            console.log("class is "+ e.className);
+           
+            if (e.className !== "drop-icon active") {
+                console.log("bye");
+                dropList.classList.remove("active");
+                links.classList.remove("active");
+         
+
+            }
+            
+            if (e.className === "lnk") {
+                // document.querySelector("#nav").classList.remove("active");
+                // document.querySelector(".icon").classList.remove("active");
+                links.classList.remove("active");
+                dropList.classList.remove("active");
+                
+            }
+            
+          
+
+
+         
+});
+
 dropList.onclick=function() {
-    links.style.display ="block";
+    // links.style.display ="block";
+    links.classList.toggle("active");
+    dropList.classList.toggle("active");
 };
-
-
-var query =  window.matchMedia("(max-width :767px)");
-if (query.matches) {
-    let home = document.querySelector('.landing');
-    let head = document.querySelector('.container');
-    links.onclick  =function(){
-        links.style.display ="none";
+// var query =  window.matchMedia("(max-width :767px)");
+// if (query.matches) {
+//     let home = document.querySelector('.landing');
+//     let head = document.querySelector('.container');
+//     links.onclick  =function(){
+//         links.style.display ="none";
         
-    };
-    home.onclick = function(){
-        links.style.display ="none";
-    };
-};
+//     };
+//     home.onclick = function(){
+//         links.style.display ="none";
+//     };
+// };
 
 
